c35432aefe6357ceb8f3948db682a7637a48656e
Raph <raphael@haddad.net.au>
2017-11-04 14:28:08 +1100

    add polyfills

--- Raph.CalorieMacroSetter/ClientApp/boot.browser.ts
+++ Raph.CalorieMacroSetter/ClientApp/boot.browser.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import 'zone.js';
 import 'bootstrap';
-import ''
+import 'mdn-polyfills/Object.assign';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module.browser';
