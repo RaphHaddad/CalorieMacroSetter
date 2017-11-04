be60e4b06fb4222cdf18dbdd5553aba307fe45a5
Raph <raphael@haddad.net.au>
2017-11-04 14:34:37 +1100

    remove object.values

--- Raph.CalorieMacroSetter/ClientApp/boot.browser.ts
+++ Raph.CalorieMacroSetter/ClientApp/boot.browser.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'zone.js';
 import 'bootstrap';
 import 'mdn-polyfills/Object.assign';
+import 'object.values'
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module.browser';
